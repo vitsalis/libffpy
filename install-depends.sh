07c2334341674cbd598dac74d8efbefe5b3e26bf
Vitalis Salis <vitsalis@gmail.com>
2018-05-01 01:26:00 +0300

    Add installation script

---
+++ install-depends.sh
@@ -0,0 +1,13 @@
+cd libffpy
+# libffpy submodules
+git submodule init && git submodule update
+cd libff
+# libff submodules
+git submodule init && git submodule update
+# install libff
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../
+make
+make install
+cd ../../../
